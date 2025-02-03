39067664cb9ef6c4be5ae7ee3b30cd31b93fa342
AbisaiMelendez <abisaimelendez@icloud.com>
2025-02-03 13:45:26 -0600

    'first'

--- models/save_client_agent.php
+++ models/save_client_agent.php
@@ -16,6 +16,16 @@ if ($mysqli->connect_error) {
     die("Conexión fallida: " . $mysqli->connect_error);
 }
 
+//configuracion de envio de correo
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
 
 function saveTransaction($data)
 {
@@ -249,3 +259,39 @@ function calculatePaymentDates($installmentsCount, $purchaseDate)
 $data = $_POST;
 
 saveTransaction($data);
+
+
+try {
+    $mail = new PHPMailer(true);
+
+    // Configuración del servidor SMTP
+    $mail->isSMTP();
+    $mail->Host = 'smtp.office365.com'; // Cambiar según tu servidor SMTP
+    $mail->SMTPAuth = true;
+    $mail->Username = 'vendor-noreply@surgepays.sv';
+    $mail->Password = 'D.460087689989az';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    // Configuración del correo
+    $mail->setFrom('vendor-noreply@surgepays.sv', 'TEST vendor');
+    $mail->addAddress('it@surgepays.sv');
+
+    // Agregar destinatarios como copia (CC)
+    $mail->addCC('etrejo@surgepays.com');
+    $mail->addCC('jsegovia@surgepays.sv');
+
+    // Agregar destinatarios como copia oculta (BCC)
+    // $mail->addBCC('auditoria@surgepays.sv');
+
+
+    $mail->Subject = 'Prueba de PHPMailer';
+    $mail->Body = 'Este es un mensaje de prueba enviado con PHPMailer.' . $data;
+
+    // Enviar correo
+    $mail->send();
+    echo 'Correo enviado correctamente.';
+} catch (Exception $e) {
+    echo "Error al enviar correo: {$mail->ErrorInfo}";
+}
+
