a016342207aa9038fd29f65ff2fe75d9312af0fd
AbisaiMelendez <abisaimelendez@icloud.com>
2025-02-03 15:53:31 -0600

    'first'

--- models/save_client_agent.php
+++ models/save_client_agent.php
@@ -313,37 +313,37 @@ $data = $_POST;
 saveTransaction($data);
 
 
-try {
-    $mail = new PHPMailer(true);
+// try {
+//     $mail = new PHPMailer(true);
 
-    // Configuración del servidor SMTP
-    $mail->isSMTP();
-    $mail->Host = 'smtp.office365.com'; // Cambiar según tu servidor SMTP
-    $mail->SMTPAuth = true;
-    $mail->Username = 'vendor-noreply@surgepays.sv';
-    $mail->Password = 'D.460087689989az';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
+//     // Configuración del servidor SMTP
+//     $mail->isSMTP();
+//     $mail->Host = 'smtp.office365.com'; // Cambiar según tu servidor SMTP
+//     $mail->SMTPAuth = true;
+//     $mail->Username = 'vendor-noreply@surgepays.sv';
+//     $mail->Password = 'D.460087689989az';
+//     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+//     $mail->Port = 587;
 
-    // Configuración del correo
-    $mail->setFrom('vendor-noreply@surgepays.sv', 'TEST vendor');
-    $mail->addAddress('it@surgepays.sv');
+//     // Configuración del correo
+//     $mail->setFrom('vendor-noreply@surgepays.sv', 'TEST vendor');
+//     $mail->addAddress('it@surgepays.sv');
 
-    // Agregar destinatarios como copia (CC)
-    $mail->addCC('etrejo@surgepays.com');
-    $mail->addCC('jsegovia@surgepays.sv');
+//     // Agregar destinatarios como copia (CC)
+//     $mail->addCC('etrejo@surgepays.com');
+//     $mail->addCC('jsegovia@surgepays.sv');
 
-    // Agregar destinatarios como copia oculta (BCC)
-    // $mail->addBCC('auditoria@surgepays.sv');
+//     // Agregar destinatarios como copia oculta (BCC)
+//     // $mail->addBCC('auditoria@surgepays.sv');
 
 
-    $mail->Subject = 'Prueba de PHPMailer';
-    $mail->Body = 'Este es un mensaje de prueba enviado con PHPMailer.' . $data['nameVendor'] . 'dates' . $dates;
+//     $mail->Subject = 'Prueba de PHPMailer';
+//     $mail->Body = 'Este es un mensaje de prueba enviado con PHPMailer.' . $data['nameVendor'] . 'dates' . $dates;
 
-    // Enviar correo
-    $mail->send();
-    echo 'Correo enviado correctamente.';
-} catch (Exception $e) {
-    echo "Error al enviar correo: {$mail->ErrorInfo}";
-}
+//     // Enviar correo
+//     $mail->send();
+//     echo 'Correo enviado correctamente.';
+// } catch (Exception $e) {
+//     echo "Error al enviar correo: {$mail->ErrorInfo}";
+// }
 
